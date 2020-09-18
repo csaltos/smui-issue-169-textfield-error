069b113d30e664924d0f87452f2bbdfed17f6122
Carlos Saltos <csaltos@gmail.com>
2020-09-18 15:02:36 +0300

    Initial code

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Textfield from '@smui/textfield';
+
+  export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<div>
+
+  <div>
+    <Textfield bind:value={name} label="Name" />
+
+    <pre class="status">Value: {name}</pre>
+  </div>
+</div>
